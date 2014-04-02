dd48a8391dc5f5f27a7dca5951c3763e7eea2ca3
Krosantos <tymko.tecca@gmail.com>
2014-04-02 18:08:05 -0400

    Reversed bubble freeze.

--- Assets/FreezeDemo/Scripts/FreezeBase.cs
+++ Assets/FreezeDemo/Scripts/FreezeBase.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class FreezeBase : MonoBehaviour {
 	
-	public bool isFrozen = true;
+	public bool isFrozen;
 	Rigidbody2D self;
 	Vector3 velocity;
 	Vector3 location;
 	void Start(){
+		isFrozen = true;
 		self = gameObject.rigidbody2D;
 		location = transform.position;
 		velocity = self.velocity;
@@ -26,12 +27,14 @@ public class FreezeBase : MonoBehaviour {
 	public void Unfreeze(){
 		if(isFrozen){
 			isFrozen = false;
+			self.gravityScale = 1;
 			self.velocity = velocity;
 		}
 	}
 
 	void Update(){
 		if(isFrozen){
+			self.gravityScale = 0;
 			transform.position = location;
 			Debug.Log ("I AM FROZEN");
 		}
