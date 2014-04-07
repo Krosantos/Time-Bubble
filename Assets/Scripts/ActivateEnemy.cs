013774cf892789966f846131c05dc1e2ac1e1b61
Noah Lemen <noah.lemen@gmail.com>
2014-04-07 17:39:12 -0400

    trigger enemies to activate them

---
+++ Assets/Scripts/ActivateEnemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateEnemy : MonoBehaviour {
+
+	void OnTriggerEnter(Collider c){
+		if (c.gameObject.tag == "Player"){
+			GetComponent<TestMob>().enabled = true;
+		}
+	}
+
+	void OnTriggerExit(Collider c){
+		if (c.gameObject.tag == "Player"){
+			GetComponent<TestMob>().enabled = false;
+		}
+	}
+}
