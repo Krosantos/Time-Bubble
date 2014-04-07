8f3f89369f48f226d4e7630ed10e772b4a77251c
Noah Lemen <noah.lemen@gmail.com>
2014-04-07 17:43:50 -0400

    trigger fix

--- Assets/Scripts/ActivateEnemy.cs
+++ Assets/Scripts/ActivateEnemy.cs
@@ -4,13 +4,13 @@ using System.Collections;
 public class ActivateEnemy : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c){
-		if (c.gameObject.tag == "Player"){
+		if (c.gameObject.tag == "EnemyActivator"){
 			GetComponent<TestMob>().enabled = true;
 		}
 	}
 
 	void OnTriggerExit(Collider c){
-		if (c.gameObject.tag == "Player"){
+		if (c.gameObject.tag == "EnemyActivator"){
 			GetComponent<TestMob>().enabled = false;
 		}
 	}
