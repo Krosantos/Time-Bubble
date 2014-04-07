31525df217887e66817b463d8cc0ec929776cd1f
Tymko Tecca <tymko.tecca@gmail.com>
2014-04-07 15:07:40 -0400

    First attempt at Mob Base Code

---
+++ Assets/Scripts/Utility/Calc.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Calc : MonoBehaviour {
+
+	public static bool isInRange(GameObject self, GameObject target, float maxDistance){
+		/*Compares the distance between a given target and the calling
+	 * instance, returning true if this value is less than the max distance specified.*/
+		Vector3 position = self.transform.position;
+		Vector3 difference = target.transform.position-position;
+		float distance = difference.sqrMagnitude;
+		if(distance<maxDistance) return true;
+		else return false;
+	}
+}
