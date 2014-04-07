2664b1f40f75a56a6442c154b9c81826b30ca3fb
Noah Lemen <noah.lemen@gmail.com>
2014-04-06 20:44:55 -0400

    fixed some problems rendering the bubble

--- Assets/LevelGenDemo/Scripts/CameraFollow.cs
+++ Assets/LevelGenDemo/Scripts/CameraFollow.cs
@@ -15,6 +15,6 @@ public class CameraFollow : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		transform.position = Vector3.Lerp(transform.position, player.transform.position, Time.deltaTime * accel);
-		transform.position = new Vector3(transform.position.x, transform.position.y, -1f);
+		transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
 	}
 }
