ffee859ac5607ef2230ce9fb10afa4d4a5fd37d5
Noah Lemen <noah.lemen@gmail.com>
2014-04-28 16:32:43 -0400

    GUI display for key boolean

---
+++ Assets/Scripts/KeyGUI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyGUI : MonoBehaviour {
+
+	MovePlayer m;
+
+	void Start(){
+		m = GameObject.FindGameObjectWithTag("Player").GetComponent<MovePlayer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (m.hasKey) renderer.enabled = true;
+	}
+}
