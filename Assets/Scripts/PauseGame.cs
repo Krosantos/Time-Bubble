67b49e965b44b7f02e7861d3b20fc750aecfb0bd
Noah Lemen <noah.lemen@gmail.com>
2014-04-29 13:14:16 -0400

    pausing!

---
+++ Assets/Scripts/PauseGame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseGame : MonoBehaviour {
+
+	bool isPaused = false;
+	LightControl lightControl;
+
+	void Start (){
+		lightControl = GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<LightControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape)) isPaused = !isPaused;
+
+		if (isPaused){
+			Time.timeScale = .000000000000000001f;
+			guiText.enabled = true;
+			lightControl.enabled = false;
+		}else{
+			Time.timeScale = 1f;
+			guiText.enabled = false;
+			lightControl.enabled = true;
+		}
+	}
+}
